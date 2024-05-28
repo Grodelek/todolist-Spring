99b765ddf972dfc31dab4fd8662153b147d49f59
Artur <arczigrodel2@wp.pl>
2024-05-28 20:27:43 +0200

    Input fields added

--- todolist/src/main/java/list/todolist/Task.java
+++ todolist/src/main/java/list/todolist/Task.java
@@ -5,8 +5,9 @@ import lombok.Data;
 
 @Data
 public class Task {
-    private String task;
-    private String date;
+    private String name;
+    private String description;
+//    private String date;
 
     public Task() {
     }
--- todolist/src/main/java/list/todolist/TaskController.java
+++ todolist/src/main/java/list/todolist/TaskController.java
@@ -3,15 +3,14 @@ package list.todolist;
 import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@Data
 public class TaskController {
     List<Task> taskList = new ArrayList<>();
 
